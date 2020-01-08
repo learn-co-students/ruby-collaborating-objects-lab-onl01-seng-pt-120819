7e0e270e691762d0e393d2308986436b99f85501
Swishyfishie <pclandreiuk@gmail.com>
2020-01-08 16:11:04 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class MP3Importer 
+    attr_accessor :path
+
+    def initialize(path)
+        @path = path
+    end 
+
+    def files 
+        arr = Dir.entries(@path)
+        arr.delete_if {|element| element == "." || element == ".."}
+    end 
+
+    def import 
+        new_array = []
+        files.each do |x|
+            new_array << x.split(' - ').join(' - ')
+        end 
+        new_array.each do |song|
+            Song.new_by_filename(song)
+        end 
+    end
+end 
--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,30 @@
-class Song
+require 'pry'
 
-    attr_accessor :name
+class Song
+    attr_accessor :name, :artist
+
+    @@all = []
     def initialize(name)
         @name = name
+        @@all << self
     end
+
+    def self.all 
+        @@all 
+    end 
+
+    def self.new_by_filename(song_name)
+        p song_name
+        artist, song = song_name.split(' - ')
+        artist = Artist.new(artist)
+        song = Song.new(song)
+        song.artist = artist
+        song
+        # new_song.artist = song
+    end 
+
+    def artist_name=(name)
+        self.artist = Artist.find_or_create_by_name(name)
+    end 
+    # binding.pry
 end
