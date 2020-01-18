354be88985378947cfebe12663b138b41d5fa1b8
cedric e goddard <8ceg6@comcast.net>
2020-01-18 03:23:33 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -21,7 +21,7 @@ def self.new_by_filename(file)
  end
  
 def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
+    self.artist = artist.find_or_create_by_name(name)
   
 end
 end 
