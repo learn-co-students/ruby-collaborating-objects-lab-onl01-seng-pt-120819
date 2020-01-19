42b3d90b566f2051fa08cc41568f90f747ebefd1
Asif Kamal <asif_kamal@hotmail.com>
2020-01-19 18:24:32 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist 
+  
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  
+  def self.all 
+    @@all 
+  end
+  
+  def add_song(song)
+    song.artist = self
+  end
+  
+  def songs 
+    self.each{ |filename| Song.new_by_filename(filename) }
+  end
+  
+end
+  
