33138353991ae188e80de5dd4cd05cb591efefca
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-22 21:58:52 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+require 'pry'
+
+class Song 
+  
+  attr_accessor :artist, :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    save 
+  end
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.new_by_filename(file_name)
+    song = file_name.split(" - ")[1]
+    new_artist = file_name.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist = Artist.new(new_artist)
+    new_song
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    # artist.add_song(self)
+  end
+  
+end
