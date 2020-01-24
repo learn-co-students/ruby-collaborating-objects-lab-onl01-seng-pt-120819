8565884ac648ff9ad838c5d699118bb0357dc8af
dwane richards <dwanerich@gmail.com>
2020-01-24 00:41:04 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,50 @@
-class Artist
-    attr_accessor :name
+require 'pry'
 
-@@all = []
+class Artist
+
+    attr_accessor :name
+    attr_reader :songs
 
   def initialize(name)
     @name = name
     @@all << self
+    @songs = []
   end
 
-  def Artist.all
+  @@all = []
+
+  def self.all
     @@all
   end
 
-  def Artist.songs
-    song.all
-  end
-
   def add_song(song)
-    @@all << song
+    @songs << song
   end
 
-  def find_or_create_by_name
-
+  def self.songs
+    songs.all
   end
 
   def print_songs
-
+      @songs.each{|s| puts s.name}
   end
 
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.find_by_name(name)
+      @@all.detect do |artist|
+        artist.name == name
+      end
+  end
+
+  def self.create_by_name(name)
+      self.new(name)
+  end
+
+
+
 end
+
+# binding.pry
--- lib/song.rb
+++ lib/song.rb
@@ -5,17 +5,23 @@ class Song
 
   def initialize(name)
     @name = name
+    @@all << self
+    @artist = nil
   end
 
   def Song.all
     @@all
   end
 
-  def new_by_filename
-
+  def self.new_by_filename(filename)
+      artist_name, song_name, genre_name = filename.chomp(".mp3").split(" - ")
+      song = self.new(song_name)
+      song.artist = Artist.find_or_create_by_name(artist_name)
+      song.genre = genre_name
   end
 
-  def artist_name=
+  def artist_name=(artist)
+    @artist = artist
 
   end
 
